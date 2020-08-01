b7b99ac4c9d44a4071c71603b48b3b922b0bf2a6
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-01 18:02:25 -0400

    change sessionstore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_nibbl_app', domain: '.powerful-springs-89951.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_nibbl_app', domain: 'powerful-springs-89951.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_nibbl_app' 
+  Rails.application.config.session_store :cookie_store, key: '_nibbl_app', domain: 'powerful-springs-89951.herokuapp.com' 
 end
