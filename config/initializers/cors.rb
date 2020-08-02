fd2b0019202f31d704b5a6e5cb27d63eab16544a
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-02 12:20:48 -0400

    configure cookies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000'
+    origins 'gavinsharkey.github.io/nibbl'
   
     resource '*',
       headers: :any,
