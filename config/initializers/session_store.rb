074e2a27df31d4c47d5d1b999f218ddff5103da2
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-02 09:54:39 -0400

    Secure cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_nibbl_app', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_nibbl_app', domain: :all, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_nibbl_app'
 end
