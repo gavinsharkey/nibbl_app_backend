e3f42e06fba8c3e970265723fea775ea9cf4ce56
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-30 16:17:31 -0400

    Configure cors Add default headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin': 'https://gavinsharkey.github.io'
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
